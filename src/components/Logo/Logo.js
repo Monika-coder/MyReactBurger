ab7e06da290dc9c4d4c7f9484b2bf869ae38b510
LNU <9march.monika@gmail.com>
2020-08-16 16:21:04 +0530

    adding first commit in local repo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import burgerlogo from '../../assets/images/burger-logo.png';
+import styles from './Logo.module.css';
+
+const logo = (props) => (
+    <div className={styles.Logo}>
+        <img src={burgerlogo} alt="MyBurger"/>
+    </div>
+);
+
+export default logo;
